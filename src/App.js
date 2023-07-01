71360d6b1214f99d0d4fd35a3aa40fd973c44c29
SiaESin <siabhon.sindab@gmail.com>
2023-06-30 22:41:38 -0700

    some styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, {useState} from 'react';
 import ColorBlock from './ColorBlock';
 import ColorForm from './ColorForm';
-
+import "./App.css";
 function App() {
-  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  let [colors, setColors] = useState(['violet', 'blue', 'teal', 'green', 'lime', 'yellow', 'orange', 'red'])
   
   const addColor = (newColor) => {
     setColors([...colors, newColor])
   }
   let colorMap = colors.map((color, i) => {
-    return(
-      <ColorBlock color={color} />
-    )
-  })
+    return <ColorBlock color={color} key={i} />;
+  });
+
   return (
     <div className="App">
-      
-    {colors.map((color, i) => 
-        <ColorBlock key={i} color={color} />
-    )}
-    <ColorForm addColor={addColor} />
-</div>
+      {colorMap}
+      <ColorForm addColor={addColor} />
+    </div>
 
   )
 }
--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -3,7 +3,10 @@ import React from "react"
 function ColorBlock(props){
     return (
         <div className="colorBlock"
-        style = {{'backgroundColor': props.color}}>
+        style = {{
+            backgroundColor: props.color,
+            height: "50px",
+            }}>
             <p>{props.color}</p>
         </div>
     )
